a2561a436224bfb66166949e323e9385da5d9888
david-maison <david.maison@hotmail.fr>
2013-06-21 10:49:14 +0200

    repouet

---
+++ src/dialog.mli
@@ -0,0 +1,13 @@
+module Right_clic_dialog : sig
+
+  type t
+  type action = (Dom_html.element Js.t, Dom_html.mouseEvent Js.t)
+           Dom_html.event_listener
+
+  val show : t -> int -> int -> unit
+  val hide : t -> unit 
+  val create : string list -> action list -> t
+
+  val hide_all : unit -> unit
+   
+end
